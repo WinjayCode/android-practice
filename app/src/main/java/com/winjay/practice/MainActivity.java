8a5a101fb7249de807e5e9c26ae565cd41168687
wenjie.duan <19900429>
2019-05-13 09:48:53 +0800

    提交测试Activity

--- app/src/main/java/com/winjay/practice/MainActivity.java
+++ app/src/main/java/com/winjay/practice/MainActivity.java
@@ -63,4 +63,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ProviderActivity.class);
         startActivity(intent);
     }
+
+    public void test(View view) {
+        Intent intent = new Intent(this, TestActivity.class);
+        startActivity(intent);
+    }
 }
