c421bcaecc85a8463b24541e33016ee8a8155a57
duanwenjie <android>
2020-08-26 23:41:51 +0800

    绘图学习

---
+++ app/src/main/java/com/winjay/practice/view/PathEffectView.java
@@ -0,0 +1,63 @@
+package com.winjay.practice.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ComposePathEffect;
+import android.graphics.CornerPathEffect;
+import android.graphics.DashPathEffect;
+import android.graphics.DiscretePathEffect;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.PathEffect;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 用各种笔触效果绘制路径
+ *
+ * @author Winjay
+ * @date 2020/8/26
+ */
+public class PathEffectView extends View {
+    private Path mPath;
+    private PathEffect[] mEffects = new PathEffect[6];
+    private Paint mPaint;
+
+    public PathEffectView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(3);
+
+        mPath = new Path();
+        mPath.moveTo(0, 0);
+        for (int i = 0; i <= 30; i++) {
+            mPath.lineTo(i * 35, (float) (Math.random() * 100));
+        }
+
+        mEffects[0] = null;
+        mEffects[1] = new CornerPathEffect(30);
+        mEffects[2] = new DiscretePathEffect(3.0F, 5.0F);
+        mEffects[3] = new DashPathEffect(new float[]{20, 10, 5, 10}, 0);
+        Path path = new Path();
+        path.addRect(0, 0, 8, 8, Path.Direction.CCW);
+        mEffects[4] = new PathDashPathEffect(path, 12, 0, PathDashPathEffect.Style.ROTATE);
+        mEffects[5] = new ComposePathEffect(mEffects[3], mEffects[1]);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        for (int i = 0; i < mEffects.length; i++) {
+            mPaint.setPathEffect(mEffects[i]);
+            canvas.drawPath(mPath, mPaint);
+            canvas.translate(0, 200);
+        }
+    }
+}
---
+++ app/src/main/java/com/winjay/practice/view/SinusoidView.java
@@ -0,0 +1,93 @@
+package com.winjay.practice.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * 正弦曲线
+ *
+ * @author Winjay
+ * @date 2020/8/26
+ */
+public class SinusoidView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+    private SurfaceHolder mHolder;
+    private Canvas mCanvas;
+    private boolean mIsDrawing;
+
+    private int x, y;
+    private Path mPath = new Path();
+    private Paint mPaint = new Paint();
+
+    public SinusoidView(Context context) {
+        super(context);
+        initView();
+    }
+
+    public SinusoidView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    public SinusoidView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    private void initView() {
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        this.setKeepScreenOn(true);
+//        mHolder.setFormat(PixelFormat.OPAQUE);
+
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(2);
+        mPaint.setStyle(Paint.Style.STROKE);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mIsDrawing = true;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mIsDrawing = false;
+    }
+
+    @Override
+    public void run() {
+        while (mIsDrawing) {
+            draw();
+            x += 1;
+            y = (int) (100 * Math.sin(x * 2 * Math.PI / 180) + 200);
+            mPath.lineTo(x, y);
+        }
+    }
+
+    private void draw() {
+        try {
+            mCanvas = mHolder.lockCanvas();
+            mCanvas.drawColor(Color.WHITE);
+            mCanvas.drawPath(mPath, mPaint);
+        } catch (Exception e) {
+        } finally {
+            if (mCanvas != null) {
+                mHolder.unlockCanvasAndPost(mCanvas);
+            }
+        }
+    }
+}
