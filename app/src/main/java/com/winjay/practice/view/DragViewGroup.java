8d41c4153fc61b504959e6e269aedcc118f8d513
wenjie.duan <19900429>
2020-10-13 20:32:08 +0800

    仿QQ侧滑菜单

---
+++ app/src/main/java/com/winjay/practice/view/DragViewGroup.java
@@ -0,0 +1,118 @@
+package com.winjay.practice.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.ViewCompat;
+import androidx.customview.widget.ViewDragHelper;
+
+import com.winjay.practice.utils.LogUtil;
+
+/**
+ * 仿QQ侧滑菜单
+ *
+ * @author Winjay
+ * @date 2020/10/13
+ */
+public class DragViewGroup extends FrameLayout {
+    private static final String TAG = DragViewGroup.class.getSimpleName();
+
+    private ViewDragHelper mViewDragHelper;
+    private View mMenuView, mMainView;
+    private int mWidth;
+
+    public DragViewGroup(@NonNull Context context) {
+        super(context);
+        initView();
+    }
+
+    public DragViewGroup(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    public DragViewGroup(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    private void initView() {
+        LogUtil.d(TAG, "initView()");
+        mViewDragHelper = ViewDragHelper.create(this, callback);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        LogUtil.d(TAG, "onFinishInflate()");
+        mMenuView = getChildAt(0);
+        mMainView = getChildAt(1);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = mMenuView.getMeasuredWidth();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        LogUtil.d(TAG, "onInterceptTouchEvent()_action=" + ev.getAction());
+        return mViewDragHelper.shouldInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // 将触摸事件传递给ViewDragHelper,此操作必不可少
+        mViewDragHelper.processTouchEvent(event);
+        return true;
+    }
+
+    private ViewDragHelper.Callback callback = new ViewDragHelper.Callback() {
+        // 何时开始检测触摸事件
+        @Override
+        public boolean tryCaptureView(@NonNull View child, int pointerId) {
+            return mMainView == child;
+        }
+
+        @Override
+        public int clampViewPositionVertical(@NonNull View child, int top, int dy) {
+            return 0;
+        }
+
+        @Override
+        public int clampViewPositionHorizontal(@NonNull View child, int left, int dx) {
+            return left;
+        }
+
+        @Override
+        public void onViewReleased(@NonNull View releasedChild, float xvel, float yvel) {
+            super.onViewReleased(releasedChild, xvel, yvel);
+            LogUtil.d(TAG, "onViewReleased()");
+            // 手指抬起后缓慢移动到指定位置
+            if (mMainView.getLeft() < 500) {
+                // 关闭菜单
+                // 相当于Scroller的startScroll方法
+                mViewDragHelper.smoothSlideViewTo(mMainView, 0, 0);
+                ViewCompat.postInvalidateOnAnimation(DragViewGroup.this);
+            } else {
+                // 打开菜单
+                mViewDragHelper.smoothSlideViewTo(mMainView, 300, 0);
+                ViewCompat.postInvalidateOnAnimation(DragViewGroup.this);
+            }
+        }
+    };
+
+    @Override
+    public void computeScroll() {
+        LogUtil.d(TAG, "computeScroll()");
+        if (mViewDragHelper.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
+    }
+}
