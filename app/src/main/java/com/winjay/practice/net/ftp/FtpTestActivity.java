41849ea6c18347bc14937d1cacd148aebc5b6157
Winjay <winjayduan@163.com>
2024-05-16 14:30:03 +0800

    null

--- app/src/main/java/com/winjay/practice/net/ftp/FtpTestActivity.java
+++ app/src/main/java/com/winjay/practice/net/ftp/FtpTestActivity.java
@@ -30,77 +30,7 @@ public class FtpTestActivity extends BaseActivity {
 
     @OnClick(R.id.ftp_btn)
     void ftpDownload() {
-        HandlerManager.getInstance().postOnSubThread(new Runnable() {
-            @Override
-            public void run() {
-                FtpUtil ftpUtils = new FtpUtil();
 
-                FTPClient ftpClient = ftpUtils.getFTPClient("ftp.axxc.cn", 21, "wftpsafe", "work2.hard");
-                String savedPath = getFilesDir().getPath();
-                LogUtil.d(TAG, "savedPath:" + savedPath);
-                if (ftpClient != null) {
-                    boolean result = ftpUtils.downLoadFTP(ftpClient, "/opt/m2mfile/ftp/", "synchronize_test.zip", savedPath);
-                    LogUtil.d(TAG, "result=" + result);
-                    ftpUtils.closeFTP(ftpClient);
-
-//                    if (result) {
-//                        try {
-//                            FileUtil.upZipFile(savedPath + "/" + "synchronize_test.zip", savedPath);
-//                        } catch (Exception e) {
-//                            e.printStackTrace();
-//                        }
-//                        String zipDirName = "";
-//                        if ("synchronize_test.zip".endsWith(".zip")) {
-//                            zipDirName = "synchronize_test.zip".replace(".zip", "");
-//                        }
-//                        Log.d(TAG, "zipDirName=" + zipDirName);
-//                        if (!TextUtils.isEmpty(zipDirName)) {
-//                            File test = new File(savedPath + File.separator + zipDirName);
-//                            if (test.exists()) {
-//                                for (File file : test.listFiles()) {
-//                                    Log.d(TAG, "dirName=" + file.getName());
-//                                    if (new File(savedPath + File.separator + file.getName()).exists()) {
-////                                        FileUtil.deleteFolder(savedPath + File.separator + file.getName());
-//                                    }
-//                                    FileUtil.copyFolder(savedPath + File.separator + zipDirName + File.separator + file.getName(),
-//                                            savedPath + File.separator + file.getName());
-//                                }
-//                            }
-//                        }
-////                        FileUtil.deleteFolder(savedPath + File.separator + zipDirName);
-////                        FileUtil.deleteFolder(savedPath + File.separator + "synchronize_test.zip");
-//                    }
-                }
-
-
-//                String savedPath = getFilesDir().getPath();
-//                LogUtil.d(TAG, "savedPath:" + savedPath);
-//                try {
-//                    FileUtil.upZipFile(savedPath + "/" + "synchronize_test.zip", savedPath);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//                String zipDirName = "";
-//                if ("synchronize_test.zip".endsWith(".zip")) {
-//                    zipDirName = "synchronize_test.zip".replace(".zip", "");
-//                }
-//                Log.d(TAG, "zipDirName=" + zipDirName);
-//                if (!TextUtils.isEmpty(zipDirName)) {
-//                    File test = new File(savedPath + File.separator + zipDirName);
-//                    if (test.exists()) {
-//                        for (File file : test.listFiles()) {
-//                            Log.d(TAG, "dirName=" + file.getName());
-//                            if (new File(savedPath + File.separator + file.getName()).exists()) {
-////                                        FileUtil.deleteFolder(savedPath + File.separator + file.getName());
-//                                FileUtil.delete(savedPath + File.separator + file.getName());
-//                            }
-//                            FileUtil.copyFolder(savedPath + File.separator + zipDirName + File.separator + file.getName(),
-//                                    savedPath + File.separator + file.getName());
-//                        }
-//                    }
-//                }
-            }
-        });
     }
 
 }
