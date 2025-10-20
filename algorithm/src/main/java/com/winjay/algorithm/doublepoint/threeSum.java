bc93f0f1f4b65800432f073ba96e812aa5f05e5c
SZ000145 <SZ000145@hnlens.com>
2025-10-20 15:36:13 +0800

    algorithm: threeSum
    Change-Id: Icc07fb0f7a4be52e57278381b850c865271f4139

--- algorithm/src/main/java/com/winjay/algorithm/doublepoint/threeSum.java
+++ algorithm/src/main/java/com/winjay/algorithm/doublepoint/threeSum.java
@@ -41,6 +41,21 @@ public class threeSum {
         int[] nums5 = new int[]{0, 0, 0, 0, -1, 0, 1, 2, -1, -4};
         int[] nums6 = new int[]{-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0};
 
+        List<List<Integer>> result1 = threeSum(nums);
+        System.out.println(result1);
+        List<List<Integer>> result2 = threeSum(nums2);
+        System.out.println(result2);
+        List<List<Integer>> result3 = threeSum(nums3);
+        System.out.println(result3);
+        List<List<Integer>> result4 = threeSum(nums4);
+        System.out.println(result4);
+        List<List<Integer>> result5 = threeSum(nums5);
+        System.out.println(result5);
+        List<List<Integer>> result6 = threeSum(nums6);
+        System.out.println(result6);
+
+
+
         List<List<Integer>> listList1 = threeSum_my(nums);
         List<List<Integer>> listList2 = threeSum_my(nums2);
         List<List<Integer>> listList3 = threeSum_my(nums3);
@@ -56,9 +71,50 @@ public class threeSum {
         System.out.println(listList6);
     }
 
+    // 时间复杂度: O(n^2)，其中 N 是数组 nums 的长度。
+    // 空间复杂度：O(logN)。我们忽略存储答案的空间，额外的排序的空间复杂度为 O(logN)。
+    // 然而我们修改了输入的数组 nums，在实际情况下不一定允许，因此也可以看成使用了一个额外的数组存储了 nums 的副本并进行排序，空间复杂度为 O(N)。
     public static List<List<Integer>> threeSum(int[] nums) {
-        return new ArrayList<>();
+        List<List<Integer>> listList = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        // 枚举 a
+        for (int first = 0; first < n; first++) {
+            // 需要和上一次枚举的数不相同
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            // c 对应的指针初始指向数组的最右端
+            int third = n - 1;
+            int target = 0 - nums[first];
+            // 枚举 b
+            for (int second = first + 1; second < n; second++) {
+                // 需要和上一次枚举的数不相同
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                // 需要保证 b 的指针在 c 的指针的左侧
+                while (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+                // 如果指针重合，随着 b 后续的增加，就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if (second == third) {
+                    break;
+                }
+
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    listList.add(list);
+                }
+            }
+        }
+        return listList;
     }
+
+
 
     // 时间复杂度: O(n^2)
     public static List<List<Integer>> threeSum_my(int[] nums) {
